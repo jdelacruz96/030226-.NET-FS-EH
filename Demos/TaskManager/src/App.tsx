9db5f922b17c7c428bf696d5f7c60c97e3be01b7
jdelacruz96 <jonathan.delacruz@revature.com>
2026-03-03 14:55:37 -0500

    finished tuesday demo

--- Demos/TaskManager/src/App.tsx
+++ Demos/TaskManager/src/App.tsx
@@ -1,14 +1,94 @@
-import { useState } from 'react'
+import { useState } from 'react' //useState is a React hook for managing state
 import TaskCard from './components/TaskCard/TaskCard'
-
+import type { Task } from './types/Task'; //import type because Task is a type
 import './App.css'
 
+// State for our TaskCard will live here - for now - we might refactor away
+// from this when we get to things like Context and useReducer later this week
+// If we were using an API to get task info - we'd send the HTTP requests from this component
+// and pass the data to TaskCard as props. For now we'll hardcode things. 
+
+//First, kind of boring, lets hardcode some task objects - as an array
+const initialTasks: Task[] = [
+  {
+    id: 1,
+    title: "Set up GH repo",
+    description: "Initialize git repo, add .gitignore for react projects",
+    assignee: "Asmita",
+    status: "todo",
+    priority: "high"
+  },
+  {
+    id: 2,
+    title: "Design DB Schema",
+    description: "Draft ERD for user, task tables",
+    assignee: "Pavel",
+    status: "in-progress",
+    priority: "medium"
+  },
+  {
+    id: 3,
+    title: "Create homepage Figma mockup",
+    description: "Sketch simple wireframe for the landing view/page",
+    assignee: "Sammar",
+    status: "todo",
+    priority: "low"
+  }
+]
+
+
 function App() {
+  //In order to store anything as State, we need to use useState (for now)
+  //state is an object, that contains two things - The State, and a Setter
+  const [tasks, setTasks] = useState<Task[]>(initialTasks)
+  
+  //Now that we have our state, and we've set it's inital value, we need to do 2 things:
+  // 1. Make sure we pass the state as a prop to our Child (TaskCard)
+  // 2. Create a callback function that will also be passed as a prop, 
+  //    so the child can send state back up to the parent
+  function handleStatusChange(id: number, newStatus: Task['status']): void {
+    // We want to iterate over our array, and create new objects that reflect
+    // the updated status when this function is called
+    setTasks((prev) => //prev refers to the exisitng (whatever is in state right now) value of state
+      
+      //Because TS is just JS with some extra stuff - we can do some pretty load bearing
+      // one-liners. When you do this, PLEASE add comments.
+      //setTasks takes a callback that receives the previous state (prev)
+      // .map() iterates over every task in the array 
+      // {...t, status: newStatus} uses the JS SPREAD OPERATOR to copy all properties
+      // of the matching task (so if I click the button to change the status of task 2, only
+      // it's info gets updated, and only the status gets overridden)
+      // This creates a new object instead of directly mutating the existing one
+      // Because react relies on that behavior (no direct mutation) to detect change and re-render
+      prev.map((t) => (t.id === id ? {...t, status: newStatus} : t))
+    )
+  }
 
   return (
-    <>
-      <TaskCard></TaskCard>
-    </>
+    <main className='app-container'>
+      <h1>Task Manger</h1>
+      <p>A Task Manager front end built with React + TS</p>
+      
+      {/* Remember, we want to render/generate a TaskCard component
+        for every task in our list (that's in State) */}
+      {/* Whenever we're using this pattern, for list of x objects
+      generate a corresponding number of components, we need to use a Key.
+      A key is a like any other prop, except we don't pass it to the child. 
+      React uses it behind the scenes, to track which child component is being 
+      updated*/}
+      <section>
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            task={task}
+            onStatusChange={handleStatusChange}
+          />
+        ))
+        }
+      </section>
+
+
+    </main>
   )
 }
 
--- Demos/TaskManager/src/components/TaskCard/TaskCard.tsx
+++ Demos/TaskManager/src/components/TaskCard/TaskCard.tsx
@@ -2,7 +2,8 @@
 // information/metadata for use in some other view
 
 import React from 'react' //every component we write will import React from 'react'
-import { Task } from '../../types/Task' //using a named import, since task did not use export default
+import type { Task } from '../../types/Task'; //using a named import, since task did not use export default
+ //using a named import, since task did not use export default
 
 // Props are just arguments that our Functional Components can take in, like any other
 // JS or TS function. We use them to pass information from Parent to Child component
@@ -12,13 +13,66 @@ import { Task } from '../../types/Task' //using a named import, since task did n
 // so they're commonly defined in line in the same file
 interface TaskCardProps {
     task: Task;
+    //Later, I will pass a function from App.tsx to this child component
+    //so that information can flow back up from child -> parent - a callback function
+    onStatusChange: (id: number, newStatus:Task["status"]) => void;
 }
 
 
-function TaskCard() {
+function TaskCard( {task, onStatusChange}: TaskCardProps ) {
 
+  //I want this taskcard to contain a button that displays the current 
+  //status of the task.  When clicked, I want to cycle through the statuses 
+  //in a specific logical order.
+  // todo -> in-progress -> done -> todo
+  // void letting the typescript compiler know that nothing gets returned
+  // from handleClick - same as function return typing in C#
+  function handleClick(): void {
+
+    //We could resolve our functionality in many different ways, thats the joy
+    //of programming. We could probably use a switch or if-elseif-else, etc
+    //We are going to use, for the sake of demo, RECORD - Typescript UTILITY TYPE
+    
+    //Record<KeyType, ValueType> - Typescript Utlity type
+    //It creates an object wjhere:
+    //  every key has to correspond to a list of set keys
+    //  every value must also correspond to a predetermined set value
+    //  We are manually enforcing what can map to what, as key-value pairs
+    const statusCycle: Record<Task["status"], Task["status"]> = {
+      //Then we manually map keys to values
+      "todo": "in-progress",
+      "in-progress": "done",
+      "done": "todo"
+    }
+
+    //Here, we will actually call upon that function that will come in as a prop from App.tsx
+    //so that we can pass the next status from Child -> Parent 
+    onStatusChange(task.id, statusCycle[task.status])
+  }
+
+  //Inside my return, I create the HTML skeleton for my component view. What does the
+  //user actually see. I can use {} to break out of HTML and into TSX so I can call upon
+  //data from my component, or even call functions from wihin my TSX return.
   return (
-    <div>TaskCard</div>
+    <div className={`task-card priority-${task.priority}`}>
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+      <div className='task-meta'>
+        <span className='assignee'>Assigned to: {task.assignee}</span>
+        
+        {/*This is a comment inside my tsx return area*/}
+        <button 
+          className={`status-badge status-${task.status}`}
+          onClick={handleClick}
+          type="button"
+          >
+            {task.status}
+        </button>
+
+      </div>
+
+
+    </div>
   )
 }
 
